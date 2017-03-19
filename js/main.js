058ebce4453bedc3076ba0e297231145018eef99
Rena Tan <rena.tanrx@gmail.com>
2017-03-18 17:44:54 -0700

    todo app, first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+function clear(evt){
+	var element = evt.srcElement.parentElement;
+	element.parentNode.removeChild(element);
+}
+
+
+function strike(evt){
+	var element = evt.target,
+		x = document.createElement('SPAN');
+	element.onclick = "";
+	element.style.textDecoration = "line-through";
+	x.classList.add('glyphicon');
+	x.classList.add('glyphicon-remove-sign');
+	x.classList.add('pull-right');
+	x.onclick = clear;
+	element.appendChild(x);
+}
+
+function todoList(){
+	var info = document.getElementById('userData').value;
+	// console.log(info);
+
+	var timeDate = document.getElementById('date').innerHTML = Date();
+	// console.log(timeDate);
+
+	var newLi = document.createElement('LI');
+	// console.log(newLi);
+
+	newLi.onclick = strike;
+
+	var newMsg = document.createTextNode(info);
+
+	newLi.appendChild(newMsg);
+	// console.log(newLi);
+
+	var list = document.getElementById('display-list');
+	list.appendChild(newLi);
+}
